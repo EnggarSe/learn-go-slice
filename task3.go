c350ea90905f1d8d13868bce63bb30ce12f31df4
EnggarSe <enggarseptrinass@gmail.com>
2020-04-28 18:32:05 +0700

    task3

---
+++ task3.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("===================Dibawah Merupakan Tugas Membedakan Isi 2 Array========================")
+	tugas3()
+	fmt.Println("=========================================================================================")
+}
+
+func tugas3() {
+	array1 := make([]int, 0)
+	maksimal := 100
+	for i := 1; i < maksimal; i++ {
+		count := 0
+		for y := 1; y < maksimal; y++ {
+			if i%y == 0 {
+				count++
+			}
+		}
+		if count == 2 && i > 1 {
+			array1 = append(array1, i)
+		}
+
+	}
+	fmt.Println(array1)
+}
